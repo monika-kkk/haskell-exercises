9281d37c17aeba2f44dcda7801676bcd5396d65d
Monika Kołtunik <monikakoltunik@gmail.com>
2017-04-24 23:47:34 +0200

    Adds simple tasks from lab1

--- src/Lab1.hs
+++ src/Lab1.hs
@@ -1,2 +1,17 @@
 module Lab1 where
 
+head' (h:_)  = h
+
+length' []  = 0
+length' (_:t) = 1 + length' t
+
+take' _ [] = []
+take' 0 _ = []
+take' n (h:t) = h : take' (n-1) t
+
+map' _ [] = []
+map' f (h:t) = f h : map' f t
+
+(+++) [] l = l
+(+++) l [] = l
+(+++) x y = (+++) (init x) (last x : y)
